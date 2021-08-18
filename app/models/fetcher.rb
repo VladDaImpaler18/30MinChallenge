3233b3fb9f504a53c3e0f0bd2f4e6f1d0384280a
Vlad <VladDaImpaler@gmail.com>
2021-08-18 10:39:37 -0400

    added error handling, begin rescue retry end

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -3,9 +3,28 @@ require 'net/https'
 class Fetcher < ApplicationRecord
   @@payload = {}
   @@base_uri = 'https://takehome.io/'
+
     def initialize(service)
-        response = Net::HTTP.get(URI(@@base_uri + service))
-        @@payload[service] = JSON.parse(response)
+        begin
+          response = Net::HTTP.get(URI(@@base_uri + service))
+          @@payload[service] = JSON.parse(response)
+
+        rescue JSON::ParserError => e
+          logger.error "ERROR: JSON ParseError -- #{e.to_s}"
+          puts "Lassy is barking again... Is Timmy trapped in a social media factory?" if e.to_s.match?(/i am trapped/i)
+          sleep 0.25
+          puts "Retrying request..."
+          retry
+
+        rescue => e
+          puts "Unexpected error!"
+          logger.unknown e.to_s
+          sleep 0.15
+          puts "Retrying request..."
+          retry
+
+        end
+
     end
     def self.getPayload
       @@payload

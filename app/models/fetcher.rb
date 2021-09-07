f185e659f54e0f0cd0a4dbcb0f9986afeebdbea6
Vlad <VladDaImpaler@gmail.com>
2021-09-07 14:02:02 -0400

    changed to ActiveModel--no need to store in database; removed redundant validations; cleaned initialize to keep with separation of duties; removed unncessary logger

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -1,42 +1,34 @@
 require 'net/http'
 require 'net/https'
-class Fetcher < ApplicationRecord
+class Fetcher
+  include ActiveModel::Model
+  attr_accessor :service, :payload
+
   validates :service, presence: true
   validates :service, format: { with: /[a-zA-Z]+/, message: "only allows for letters"}
-  @@payload = {}
+
   @@base_uri = 'https://takehome.io/'
 
     def initialize(service)
       super
-        begin
-          response = Net::HTTP.get(URI(@@base_uri + self.service))         
-          @@payload[self.service] = JSON.parse(response)
-
-          rescue JSON::ParserError => e
-            logger.error "ERROR: JSON ParseError -- #{e.to_s.split("\n").first}"
-            if e.to_s.match?(/i am trapped/i)
-              puts "Lassy is barking again... Is Timmy trapped in a social media factory?" 
-              sleep 0.5
-              puts "Retrying request..."
-              retry
-            end
-            
-          rescue TypeError => e
-            if self.service.nil?
-              logger.error "ERROR: Missing required parameter. 0 of 1"
-            elsif self.service.class != "".class
-              logger.error "Error: Incorrect parameter type, must be a string"
-            else
-              logger.error e.to_s.split("\n").first
-            end
-
-          rescue => e
-            puts "Unexpected error!"
-            logger.unknown e.to_s.split("\n").first
-
-        end
+      get_data(service) unless errors.any?
+      # valid? Default behavior clears error messages. https://github.com/rails/rails/blob/8ee716182df410052707c3d1eabf8bfd8e0e1c5e/activemodel/lib/active_model/validations.rb#L335-L343
     end
-    def self.getPayload
-      @@payload
+
+    def get_data(service)
+      begin
+        response = Net::HTTP.get(URI(@@base_uri + @service))         
+        @payload = JSON.parse(response)
+
+        rescue JSON::ParserError => e
+          errors[:payload] << "JSON ParseError -- #{e.to_s.split("\n").first}"
+          if e.to_s.match?(/i am trapped/i)
+            puts "Lassy is barking again... Is Timmy trapped in a social media factory?" 
+          end
+
+        rescue => e
+          errors[:base] << "Unexpected error! #{e.to_s.split("\n").first}"
+      end
     end
+
 end
